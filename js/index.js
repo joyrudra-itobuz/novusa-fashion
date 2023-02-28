811adc5b0ca2d3bc9677f960f5bc0303daee2331
rahulkarmakaritobuz <rahul.karmakar@itobuz.com>
2023-02-28 12:57:04 +0530

    Bestseller & Aesthetic

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// Rahul start
+let swiper = new Swiper(".mySwiper", {
+slidesPerView: "3.3",
+spaceBetween: 30,
+autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+},
+pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+breakpoints: {
+    0: {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    },
+    425: {
+    slidesPerView: 1.5,
+    spaceBetween: 30,
+    },
+    575: {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    },
+    768: {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    },
+    1024: {
+    slidesPerView: 3.5,
+    spaceBetween: 50,
+    },
+},
+});
+//Rahul end
